9366550449880568b8f8e17a0859cd67c36c6375
Srim <robertrobinson777@gmail.com>
2018-01-11 18:38:22 +0530

    firebase changesx

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
 import 'hammerjs';
 
 if (environment.production) {
